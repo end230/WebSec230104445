0cad9d630626649277ee5b4cea64c01c2762fd37
end230 <thenaughtyboooy@gmail.com>
2025-02-22 20:58:37 +0200

    z

--- Web_Sec/routes/web.php
+++ Web_Sec/routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/Odd', function () {
+    return view('Odd');
+});
+Route::get('/Prime', function () {
+    return view('Prime');
+});
+Route::get('/Multiplication', function () {
+    return view('Multiplication');
+});
