de9eb2add34c165b768ab6330e97d01645067378
VB0i <zezosuliman95@gmail.com>
2025-02-22 17:12:54 +0200

    Done1

--- WebSecService/resources/views/welcome.blade.php
+++ WebSecService/resources/views/welcome.blade.php
@@ -16,9 +16,31 @@
 
 
 </head>
-    <body class="font-sans antialiased dark:bg-black dark:text-white/50">
-    <h1>Welcome Page</h1>
-    <p>To go any of multable,even or prime numbers page</p>
-    <p>Type in the URL</p>
+<body class="font-sans antialiased dark:bg-black dark:text-white/50">
+
+<nav class="navbar navbar-expand-sm bg-light">
+<div class="container-fluid">
+<ul class="navbar-nav">
+<li class="nav-item">
+<a class="nav-link" href="./">Home</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="./even">Even Numbers</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="./prime">Prime Numbers</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="./multable">Multiplication Table</a>
+</li>
+</ul>
+</div>
+</nav>
+<div class="card m-4">
+<div class="card-body">
+Welcome to Home Page
+</div>
+</div>
+</body>
     </body>
 </html>
